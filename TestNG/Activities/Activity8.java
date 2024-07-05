318e68dd17c86f03b0c1d4c5d763f9083d4a033a
vinaydt1 <vinaydt1@in.ibm.com>
2024-07-05 16:49:21 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity8.java
@@ -0,0 +1,23 @@
+package activities;
+
+public class Activity8 {
+
+	public static void main(String[] args) {
+
+		System.out.println("As mentioned, testng.xml file has been created to run the activities in parallel.");
+
+		/*
+		 * <?xml version="1.0" encoding="UTF-8"?> <!DOCTYPE suite SYSTEM
+		 * "https://testng.org/testng-1.0.dtd" > <suite name="TestSuite"
+		 * parallel="classes"> <test name="SuiteTests" > <parameter name="username"
+		 * value="admin"/> <parameter name="password" value="password"/> <classes>
+		 * <class name="activities.Activity1"/> <class name="activities.Activity2"/>
+		 * <class name="activities.Activity3"/> <class
+		 * name="activities.Activity4_DemoOne"/> <class
+		 * name="activities.Activity4_DemoTwo"/> <class name="activities.Activity5"/>
+		 * <class name="activities.Activity6"/>
+		 * </classes> </test> </suite>
+		 */
+	}
+
+}
